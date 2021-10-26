404e96ba3ff746a0731cc8afb1e0bdc495439471
remorses <beats.by.morse@gmail.com>
2021-10-26 14:23:49 +0200

    pages/api/throw:

--- pages/api/throw.js
+++ pages/api/throw.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
     console.log('stdout')
     console.error('stderr')
+    res.status(200).send('Hello World!')
     throw new Error('Something went wrong')
 }
