4a29aeb4c4cfacf72e2d8b09baca9fb59ce876b4
remorses <beats.by.morse@gmail.com>
2021-10-26 14:17:51 +0200

    pages/api: fix stuff

--- pages/api/dontThrow.js
+++ pages/api/dontThrow.js
@@ -2,5 +2,5 @@ export default function handler(req, res) {
     console.log('stdout')
     console.error('stderr')
 
-    res.status(200).text('hello')
+    res.status(200).send('hello')
 }
--- pages/api/errStatus.js
+++ pages/api/errStatus.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
     console.log('stdout')
     console.error('stderr')
-    res.status(500).text('error')
+    res.status(500).send('error')
 }
